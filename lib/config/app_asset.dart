4f15a53c370641731e48419be5c299911c040463
wibisanabama <wibisanabama@gmail.com>
2026-04-27 07:51:26 +0700

    Add AppAssets class for managing food asset paths

---
+++ lib/config/app_asset.dart
@@ -0,0 +1,12 @@
+class AppAssets{
+  static const food1 = 'assets/food_01.png';
+  static const food2 = 'assets/food_02.png';
+  static const food3 = 'assets/food_03.png';
+  static const food4 = 'assets/food_04.png';
+  static const food5 = 'assets/food_05.png';
+  static const food6 = 'assets/food_06.png';
+  static const food7 = 'assets/food_07.png';
+  static const food8 = 'assets/food_08.png';
+  static const food9 = 'assets/food_09.png';
+  static const food10 = 'assets/food_10.png';
+}
